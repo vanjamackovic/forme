be9c133632df4bc74804f7c06e74cc7f834551c2
vanjamackovic <vanja.mackovic@skole.hr>
2016-03-08 17:33:43 +0100

    procistili smo php.

--- index.php
+++ index.php
@@ -5,21 +5,6 @@
     <title> Naslov </title>
 </head>
 <body>
-<hl> Prvi</hl>
-<p>
-    <?php
-    echo('Pozdrav is PHP-a.');
-    echo('<br>');
-    echo(date('d.m.Y.'));
-    ?>
-</p>
-<p>
-    <?php
-	$date1 = ('d.m.Y.');
-	$date2 = ('18.03.2016.');
-	$diff = $date2 - $date1;
-    echo('Do praznika je ostalo još: <b>'.$diff.'</b>');
-    ?>
-</p>
+<h1>Forme</h1>
 </body>
 </html>
